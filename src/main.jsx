26bccac5e98c9d7af634d6ea8686495f6c884f80
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-09-29 21:28:43 +0200

    Add pages for posts index and individual post view

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { PostsIndex } from './pages/PostsIndex';
+import PostDetail from './pages/PostDetail';
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <PostsIndex />
+  },
+  {
+    path: '/post/:id',
+    element: <PostDetail />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/pages/PostsIndex.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import ArticleOverview from "../components/ArticleOverview";
+
+
+export const PostsIndex = () => {
+  const [allPosts, setAllPosts] = useState();
+
+  const fetchPosts = async () => {
+    const res = await fetch('https://blogapi-production-5dee.up.railway.app/posts');
+    const data = await res.json();
+    setAllPosts(data);
+  }
+
+  const postsList = () => {
+    if (!allPosts) {
+      return null;
+    } else {
+      return (
+        <>
+          {
+            allPosts.map((el) => {
+              return (
+                <ArticleOverview key={el._id} blogPost={el} />
+              )
+            })
+          }
+        </>
+      )
+    }
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return (
+    <>
+      {postsList()}
+    </>
+  )
+
+}
