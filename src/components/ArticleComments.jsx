2dc00166013eb07f22b6be01aafbc0078d751d56
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-10-02 16:24:55 +0200

    Add delete comment function

--- src/components/ArticleComments.jsx
+++ src/components/ArticleComments.jsx
@@ -1,9 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+
 const IndividualComment = ({ comment }) => {
+  const navigateTo = useNavigate();
+  const { id } = useParams();
+
+
+  const deleteComment = async () => {
+    if (window.confirm("Do you really want to delete this comment?")) {
+      await fetch(`https://blogapi-production-5dee.up.railway.app/comments/${comment._id}/delete`, 
+      { method: 'DELETE' });
+      location.reload();
+    } else {
+      return;
+    }
+  }
+
   return (
     <div className="individual-comment_container">
       <p>{comment.author.username}</p>
       <p>{comment.text}</p>
       <p>{comment.createdAt_formatted}</p>
+      <button onClick={deleteComment}>Delete comment</button>
     </div>
   )
 }
